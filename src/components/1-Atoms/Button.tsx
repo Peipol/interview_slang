e95491c9996ad9936d1d2fdd44f717725cf29a90
Peipol <peipolthings@gmail.com>
2022-03-11 16:30:30 -0400

    Buttom Color correction

--- src/components/1-Atoms/Button.tsx
+++ src/components/1-Atoms/Button.tsx
@@ -8,25 +8,28 @@ interface ButtonProps {
 
 const hoverBlur : number = 10;
 const activeBlur : number = 20;
-
+const pink = "#fb80be";
 const StyledButton = styled.button<ButtonProps>`
-  background-color: palevioletred;
+  background-color: ${pink};
   color: white;
   font-weight: bold;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid ${pink};
   border-radius: 8px;
-  box-shadow: 0px 0px 1.8px #fff, 2px 2px 3px #ff6bb4, 2px 3px 5px #ff9ecf,
-    2px 3px 8px #ffd1e8;
+  box-shadow: 
+  1px 2px 3px #fff,
+  2px 2px 4px #ff6bb4,
+  2px 3px 5px #ff9ecf,
+  2px 3px 10px #ffd1e8;
 
   &:hover {
     background-color: #ebadc2;
     border-color: #ebadc2;
     color: #282c34;
     box-shadow: 
-    0px 0px ${1.8 + hoverBlur}px #fff, 
+    0px 0px ${3 + hoverBlur}px #fff, 
     2px 2px ${3   + hoverBlur}px #ff6bb4,
     2px 3px ${5   + hoverBlur}px #ff9ecf, 
     2px 3px ${8   + hoverBlur}px #ffd1e8;
@@ -35,7 +38,7 @@ const StyledButton = styled.button<ButtonProps>`
     color: rgba(40, 44, 52, 0.50);
     background-color: #efbece;
     box-shadow: 
-    0px 0px ${1.8 + activeBlur}px #fff,
+    0px 0px ${3 + activeBlur}px #fff,
     2px 2px ${3   + activeBlur}px #ff6bb4,
     2px 3px ${5   + activeBlur}px #ff9ecf,
     2px 3px ${8   + activeBlur}px #ffd1e8;
